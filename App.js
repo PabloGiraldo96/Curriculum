5875f393ab8f2d4efc2554f5b432b8eb046efabc
Juan Pablo Jaramillo Giraldo <juanpablogiraldo9602@gmail.com>
2023-02-26 23:07:15 -0500

    Se agregan archivos

---
+++ App.js
@@ -0,0 +1,27 @@
+
+const myTimeout = setTimeout(displayFooter, 10000);
+
+function displayFooter(){
+
+let footer = document.getElementById('footer');
+footer.style.display="block";
+}
+
+myTimeout();
+
+function getRandomColor() {
+	let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++ ) {
+         color += letters[Math.floor(Math.random() * 14)];
+    }
+    return color;
+}
+
+function title() {
+          document.getElementById("h1").style.color = getRandomColor();
+		  document.getElementById("h1").style.transition = "all 0.5s ease";
+          setTimeout(title, 2500);
+      }
+
+	title();
